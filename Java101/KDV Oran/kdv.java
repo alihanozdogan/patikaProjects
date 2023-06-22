5372a5b02636971306fdb051e147e2d93b0f4456
alihanozdogan <65493677+alihanozdogan@users.noreply.github.com>
2023-06-23 02:12:31 +0300

    Create kdv.java

---
+++ Java101/KDV Oran/kdv.java
@@ -0,0 +1,21 @@
+package java101;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class kdv {
+    public static void main(String[] args) {
+        double fiyat,kdvTutarı,kdvliFiyat,kdvOran=0.18;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Fiyat giriniz : ");
+        fiyat = scanner.nextDouble();
+        if(fiyat <=1000 && fiyat >0)kdvOran = 0.18;
+        else kdvOran = 0.08;
+        kdvTutarı = fiyat * kdvOran;
+        kdvliFiyat = fiyat + kdvTutarı;
+        System.out.println("Kdv'siz Fiyat : " + fiyat);
+        System.out.println("Kdv Oranı : " + kdvOran);
+        System.out.println("Kdv Tutarı : " + kdvTutarı);
+        System.out.println("Kdvli Fiyat : " + kdvliFiyat);
+    }
+}
