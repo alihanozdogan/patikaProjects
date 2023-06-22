f206ee76864c7e9207a6ed9d6a89b9599bd304a2
alihanozdogan <65493677+alihanozdogan@users.noreply.github.com>
2023-06-23 01:58:57 +0300

    Create Main.java

---
+++ Java101/Not ortalaması/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Matematik notunuz");
+        mat = scanner.nextInt();
+        System.out.print("Fizik notunuz");
+        fizik = scanner.nextInt();
+        System.out.print("Kimya notunuz");
+        kimya = scanner.nextInt();
+        System.out.print("Turkce notunuz");
+        turkce = scanner.nextInt();
+        System.out.print("Tarih notunuz");
+        tarih = scanner.nextInt();
+        System.out.print("Muzik notunuz");
+        muzik = scanner.nextInt();
+        scanner.close();
+        double ortalama = (mat + fizik + kimya + turkce+tarih+muzik)/6.0;
+        System.out.println("Ortalamanız " +ortalama);
+        String gecme = ortalama >= 60 ? "Geçtiniz" : "Kaldınız";
+        System.out.println("Geçme durumunuz :" + gecme);
+
+
+    }
+}
