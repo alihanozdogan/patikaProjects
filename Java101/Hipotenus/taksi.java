d6426aaa1a100aa5cfa10b478998a17b16a33c5e
alihanozdogan <65493677+alihanozdogan@users.noreply.github.com>
2023-07-07 23:32:36 +0300

    changes

--- Java101/Hipotenus/taksi.java
+++ Java101/Hipotenus/taksi.java
@@ -1,7 +1,16 @@
 package java101;
 
+import java.util.Scanner;
+
 public class taksi {
     public static void main(String[] args) {
+    double tutar,km,acilis=10;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Km giriniz; ");
+        km = sc.nextInt();
+        tutar = acilis + km*2.20;
+        if(tutar < 20)tutar=20;
 
+        System.out.println("Toplam tutar "+ tutar);
     }
 }
